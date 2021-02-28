3ac916b2d55902e217c660ea6925fd53828a8c39
Umesh Tyagi <umeshtyagi829@gmail.com>
2021-02-28 21:39:35 +0530

    Fourth Version

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello, Everyone!, This is Umesh</h1>"
+    return "<h1>Hello, Everyone!, This is Umesh  <img src='https://imagest.blob.core.windows.net/myc12/images.jpg' width='200'  height='300'/></h1>"
